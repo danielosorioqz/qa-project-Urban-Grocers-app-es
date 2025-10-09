5e2c1f0f922bd0b396cf9694f1246eb80a353963
Daniel Osorio Quiroz <daniel.osorioqz@gmail.com>
2025-10-09 15:00:05 -0500

    configuration

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-e3fbbc90-c126-42c7-9ae2-484b2bd04135.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-f8f0bbaa-3c42-4b67-8e08-6ff633f87c09.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
