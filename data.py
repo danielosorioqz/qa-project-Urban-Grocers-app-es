d50068b7135e247406f04e5ce3b9eefb0fa6c91e
Daniel Osorio Quiroz <daniel.osorioqz@gmail.com>
2025-10-09 15:28:36 -0500

    configuration

---
+++ data.py
@@ -0,0 +1,46 @@
+headers = {
+    "Content-Type": "application/json",
+
+}
+
+body = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
+
+kit_body = {
+    'name': ''
+}
+
+kit_body_one_letter = {
+    'name': 'a'
+}
+
+kit_body_511_letters = {
+    'name': 'AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC'
+}
+
+kit_body_512_letters = {
+    'name': 'AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD'
+}
+
+kit_body_caracteres_especiales = {
+    'name': '"№%@",'
+}
+
+kit_body_espacios = {
+    'name': 'A Aaa'
+}
+
+kit_body_numeros = {
+    'name': '123'
+}
+
+kit_body_sin_parametro = {
+
+}
+
+kit_body_parametro_diferente = {
+    'name': 123
+}
