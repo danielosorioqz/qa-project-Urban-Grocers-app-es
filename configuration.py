c59db9f14218fac5e7d349bb93d80665244e7296
Daniel Osorio Quiroz <daniel.osorioqz@gmail.com>
2025-10-13 23:01:05 -0500

    ultima prueba nueva URL del servidor

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-fda73651-27fa-4e0c-a03e-6e8033c1667c.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-3df9b558-a0fb-4f6a-ac51-1e0ddcc8cf51.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
